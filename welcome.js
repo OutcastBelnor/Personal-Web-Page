e87fb256dec9c4a4fbabeb1d31f959c29a754ae5
OutcastBelnor <jakub.janas08@gmail.com>
2016-11-06 12:10:18 +0000

    Placed all the files in the repository

---
+++ welcome.js
@@ -0,0 +1,35 @@
+function welcomeByName()
+{
+	var name = document.getElementById("name").value;
+	
+	var date = new Date();
+	var hours = date.getHours();
+	
+	if (name != "")
+	{
+		if (hours < 12)
+		{
+			alert("Good morning " + name + "!");
+			document.getElementById("welcome").innerHTML = 
+				"<h1>Good morning " + name + "!</h1><br />Make yourself comfortable!";
+		}
+		else if (hours < 18)
+		{
+			alert("Good afternoon " + name + "!");
+			document.getElementById("welcome").innerHTML = 
+				"<h1>Good afternoon " + name + "!</h1><br />Make yourself comfortable!";
+		}
+		else
+		{
+			alert("Good evening " + name + "!");
+			document.getElementById("welcome").innerHTML = 
+				"<h1>Good evening " + name + "!</h1><br />Make yourself comfortable!";
+		}
+
+		if (name == "Agatka" || name == "Agata")
+		{
+			document.getElementById("welcome").innerHTML = 
+			"<h1>Welcome " + name + "!</h1><br />KOCHAM CIĘ ;*";
+		}
+	}
+}
